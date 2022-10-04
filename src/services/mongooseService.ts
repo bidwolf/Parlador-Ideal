7bca125e661665f565eedd4aa34a2e51ac4e60da
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-10-04 17:27:20 -0300

    modificando rota auth para utilizar os controllers

---
+++ src/services/mongooseService.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { MONGODB_USER } = process.env;
+const MONGODB_PASSWORD = encodeURIComponent(process.env.MONGODB_PASSWORD ? process.env.MONGODB_PASSWORD : false) || process.env.MONGODB_PASSWORD;
+const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.4p6vxw7.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(MONGODB_URI)
+.then(() => {
+  console.log("Mongo db conectado");
+})
+.catch(err => console.error(err));
+export default mongoose;
