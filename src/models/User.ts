bf889a2743851f4f5897dfaf23895e610bcf2533
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-10-06 20:05:37 -0300

    Criando relacionamento entre o usuário e o token de autenticação

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,10 @@
 import { model, Schema } from 'mongoose'
 
+import { refreshToken } from './refreshToken'
 export interface UserLogin {
   email: string
   password: string
-  token?: string
+  refreshToken?: refreshToken
 }
 export interface UserDTO extends UserLogin {
   name: string
@@ -15,8 +16,8 @@ const UserSchema = new Schema<UserDTO>({
   email: { type: String, required: true },
   name: { type: String, required: true },
   password: { type: String, required: true },
-  token: {
-    type: 'string',
+  refreshToken: {
+    type: Schema.Types.ObjectId,
     ref: 'refreshToken',
     required: false,
   },
