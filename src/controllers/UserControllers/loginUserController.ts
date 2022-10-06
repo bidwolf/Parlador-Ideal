e41bafb223a1a77c201a6958838eda75a6e808f5
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-10-06 20:09:57 -0300

    Adicionando funções auxiliares

--- src/controllers/UserControllers/loginUserController.ts
+++ src/controllers/UserControllers/loginUserController.ts
@@ -1,7 +1,9 @@
 import { compare as bcrypt_compare } from 'bcrypt'
 import { Request, Response } from 'express'
 import { sign as jwt_sign } from 'jsonwebtoken'
+import { ObjectId } from 'mongoose'
 
+import refreshTokenModel from '../../models/refreshToken'
 import UserModel, { UserLogin } from '../../models/User'
 const generateToken = (userId: string, expiresIn: string) => {
   const SECRET = process.env.SECRET || ''
@@ -10,6 +12,34 @@ const generateToken = (userId: string, expiresIn: string) => {
     expiresIn: expiresIn,
   })
   return token
+}
+const validateLogin = async (authUser: UserLogin) => {
+  const user = await UserModel.findOne<UserLogin>({ email: authUser.email })
+  if (!user) {
+    throw { code: 404, message: 'Email invalid!' }
+  }
+  const checkPassword = await bcrypt_compare(authUser.password, user.password)
+  if (!checkPassword) {
+    throw { code: 422, message: 'Invalid password!' }
+  }
+}
+const createRefreshToken = async (tokenId: ObjectId, email: string) => {
+  const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || ''
+  const user = await UserModel.findOne({ email }, '-password')
+  const token = jwt_sign({ id: user?._id }, REFRESH_TOKEN_SECRET, {
+    subject: user?.id,
+    expiresIn: '20s',
+  })
+  const refreshToken = await refreshTokenModel.findById(tokenId)
+  if (!refreshToken) {
+    const newRefreshToken = await refreshTokenModel.create({
+      user,
+      expiresAt: Date.now(),
+      token,
+    })
+    user?.updateOne({ $set: { refreshToken: newRefreshToken } })
+  }
+  console.log(refreshToken?.expiresAt)
 }
 export default async function login(
   req: Request,
@@ -38,7 +68,7 @@ export default async function login(
   try {
     const token = generateToken(user.id, '60s')
     const refreshToken = jwt_sign(user, REFRESH_TOKEN_SECRET)
-    await user.updateOne({ $set: { token: token } })
+    await user.updateOne({ $set: { refreshToken: refreshToken } })
     return res
       .status(200)
       .json({ code: 200, message: 'Authentication done successfully', token })
