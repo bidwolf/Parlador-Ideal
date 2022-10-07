cb04dd23e7976f6724ba44c49a311bedb9d0e67e
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-10-07 16:31:18 -0300

    Removendo rota de registro de usuário da rota de autenticação e adicionando-a na rota de usuário

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express'
 
 import { refreshToken } from '../controllers/TokenControllers/tokenController'
-import createUserController from '../controllers/UserControllers/createUserController'
 import login from '../controllers/UserControllers/loginUserController'
 const router = Router()
-router.post('/register', createUserController)
 router.post('/login', login)
 router.post('/token', refreshToken)
 export default router
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser'
 import { Router } from 'express'
 import session from 'express-session'
 
+import createUserController from '../controllers/UserControllers/createUserController'
+import { deleteUser } from '../controllers/UserControllers/deleteUserController'
 import getUser from '../controllers/UserControllers/getUserController'
 import { logout } from '../controllers/UserControllers/logoutUserController'
 import { ensureAuthenticated } from '../middlewares/EnsureAuthenticated'
@@ -17,8 +19,9 @@ const SECRET = process.env.SECRET || ''
 const router = Router()
 router.use(cookieParser(SECRET))
 router.use(session({ secret: SECRET, saveUninitialized: false, resave: true }))
-
+router.post('/register', createUserController)
 router.use(ensureAuthenticated)
 router.get('/:id', getUser)
 router.delete('/logout', logout)
+router.delete('/:id/deleteUser', deleteUser)
 export default router
