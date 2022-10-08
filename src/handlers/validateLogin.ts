7bd80afb8daa95ee3c67b3fc87be3039e7d35158
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-10-08 18:01:48 -0300

    Criando handlers para facilitar o reaproveitamento de código e criação de rota update user genérica

---
+++ src/handlers/validateLogin.ts
@@ -0,0 +1,13 @@
+import { compare } from 'bcrypt'
+
+import UserModelDTO, { UserLogin } from '../models/User'
+export const validateLogin = async (authUser: UserLogin) => {
+  const user = await UserModelDTO.findOne<UserLogin>({ email: authUser.email })
+  if (!user) {
+    throw { code: 404, message: 'Email invalid!' }
+  }
+  const checkPassword = await compare(authUser.password, user.password)
+  if (!checkPassword) {
+    throw { code: 422, message: 'Invalid password!' }
+  }
+}
