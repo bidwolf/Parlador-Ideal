f50aa85831ce04430f6d62bd0d146bd367d3da8d
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-10-10 10:10:23 -0300

    adicionando rota logout

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 
 import login from '../controllers/AuthControllers/loginUserController'
+import { logout } from '../controllers/AuthControllers/logoutUserController'
 const router = Router()
 router.post('/login', login)
+router.post('/logout', logout)
 export default router
