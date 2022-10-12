af7c7fb04ca2710921c012f27a9ca233717a1f46
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-10-12 00:26:05 -0300

    Adicionando Heading padronizado ao projeto

--- parladorIdealApp/src/components/InputForm/index.tsx
+++ parladorIdealApp/src/components/InputForm/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { TextInputProps, View } from 'react-native';
 import Feather from'react-native-vector-icons/Feather'
 import { THEME } from '../../theme';
-import { Container } from './styles';
+import { Container, IconContainer, InputText } from './styles';
 export type InputProps = TextInputProps & {
   icon: React.ComponentProps<typeof Feather>['name'];
   value?: string;
@@ -20,7 +20,16 @@ export function InputForm({icon,value,...rest}:InputProps) {
   }
   return (
     <Container>
-      <Feather name={icon} size = {32} color ={THEME.COLORS.CAPTION_300} />
+      <IconContainer isFocused={isFocused}>
+      <Feather name={icon} size = {32} color ={(isFocused||isFilled)?THEME.COLORS.CAPTION_300:THEME.COLORS.CAPTION_500} />
+      </IconContainer>
+      <InputText
+      onFocus={handleInputFocus}
+      onBlur={handleInputBlur}
+      isFocused={isFocused}
+      value={value}
+      {...rest}
+      />
     </Container>
   );
 }
--- parladorIdealApp/src/components/InputForm/styles.ts
+++ parladorIdealApp/src/components/InputForm/styles.ts
@@ -1,6 +1,32 @@
 import { StyleSheet } from 'react-native';
 import styled,{css} from 'styled-components/native'
+interface Props{
+  isFocused: boolean
+}
 export const Container = styled.View`
 flex-direction:row;
-margin-Bottom:8px;
+margin-left: 16px;
+margin-Bottom:24px;
 `
+export const InputText = styled.TextInput<Props>`
+flex: 1;
+  background-color: #121214;
+  color: #7A7A80;
+  padding: 0 16px;
+  margin-right: 48px;
+  ${({ isFocused }) => isFocused && css`
+    border-width: 1px;
+    border-color: #2A95B8;    
+  `};
+`;
+export const IconContainer = styled.View<Props>` height: 48px;
+width: 48px;
+justify-content: center;
+align-items: center;
+margin-right: 4px;
+
+${({ isFocused }) => isFocused && css`
+  border-left-width: 1px;
+  border-left-color: #2A95B8;   
+`};
+`;
